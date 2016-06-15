a23d15c9ac3b64f3070a6eae7f3fc6f5d31de6c5
Nigel O'Toole <pure@purestructure.com>
2016-06-15 13:14:55 +0100

    Added JS to show path animation and added theming

--- app/scripts/site.js
+++ app/scripts/site.js
@@ -1,1 +1,54 @@
+'use strict';
+
 // This JS is only needed for the demo to show features
+
+var progressTrackerDemo = (function() {
+
+	function initAnimPathDemo() {
+
+		var animPathLinks = document.querySelectorAll('#anim-path li');
+		var animPathLinksLength = animPathLinks.length;
+
+		for (var i = 0; i < animPathLinksLength; i++) {
+			_handleClick(animPathLinks[i]);
+		}
+
+	}
+
+	function _handleClick(link) {
+		link.addEventListener('click', function(e) {
+			e.preventDefault();
+			_toggleClass(this, 'is-complete');
+
+			if(this.nextElementSibling !== null) {
+				_toggleClass(this.nextElementSibling, 'is-active');
+			}
+
+		});
+	};
+
+	function _toggleClass(el, className) {
+
+		if (el.classList) {
+			el.classList.toggle(className);
+		} else {
+			var classes = el.className.split(' ');
+			var existingIndex = classes.indexOf(className);
+
+			if (existingIndex >= 0)
+			classes.splice(existingIndex, 1);
+			else
+			classes.push(className);
+
+			el.className = classes.join(' ');
+		}
+
+	}
+
+	return {
+		initAnimPathDemo: initAnimPathDemo
+	};
+
+})();
+
+progressTrackerDemo.initAnimPathDemo();

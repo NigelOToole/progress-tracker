ecfee2eff0317c4b53ae6448e1d8ab256ae2b38c
Nigel O'Toole <pure@purestructure.com>
2016-06-16 12:10:52 +0100

    Rearrange styles and minor updates

--- app/scripts/site.js
+++ app/scripts/site.js
@@ -4,20 +4,21 @@
 
 var progressTrackerDemo = (function() {
 
+	var animPathLinks = document.querySelectorAll('#anim-path li');
+	var animPathLinksLength = animPathLinks.length;
+
 	function initAnimPathDemo() {
 
-		var animPathLinks = document.querySelectorAll('#anim-path li');
-		var animPathLinksLength = animPathLinks.length;
-
 		for (var i = 0; i < animPathLinksLength; i++) {
-			_handleClick(animPathLinks[i]);
+			_handleClick(animPathLinks[i], i);
 		}
 
 	}
 
-	function _handleClick(link) {
+	function _handleClick(link, index) {
 		link.addEventListener('click', function(e) {
 			e.preventDefault();
+			_deactivateOtherLinks(index);
 			_toggleClass(this, 'is-complete');
 
 			if(this.nextElementSibling !== null) {
@@ -25,6 +26,16 @@ var progressTrackerDemo = (function() {
 			}
 
 		});
+	};
+
+	function _deactivateOtherLinks(activeIndex) {
+
+		for (var i = 0; i < animPathLinksLength; i++) {
+			if (i >= activeIndex) {
+				_removeClass(animPathLinks[i], 'is-complete');
+				_removeClass(animPathLinks[i], 'is-active');
+			}
+		}
 	};
 
 	function _toggleClass(el, className) {
@@ -42,6 +53,15 @@ var progressTrackerDemo = (function() {
 
 			el.className = classes.join(' ');
 		}
+
+	}
+
+	function _removeClass(el, className) {
+
+		if (el.classList)
+		  el.classList.remove(className);
+		else
+		  el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
 
 	}
 
